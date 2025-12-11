f9dea07e0325bd72056b4b7d44c9f047c4428f93
priyamore2018 <piya.more2018@gmail.com>
2025-12-11 15:06:00 +0530

    FirstPractice_Commit

---
+++ Java_GitHub_PracticeExample/src/org/java/gitpractice/test/priyanka/TestHelloProgram.java
@@ -0,0 +1,9 @@
+package org.java.gitpractice.test.priyanka;
+
+public class TestHelloProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");	}
+
+}
